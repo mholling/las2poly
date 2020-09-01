e5d55af75c26b77115958e30adcd1667390c4967
Matthew Hollingworth <mdholling@gmail.com>
2020-09-01 19:22:09 +1000

    check PLY header more rigorously

--- src/ply.hpp
+++ src/ply.hpp
@@ -22,32 +22,42 @@ class PLY {
 		return test.bytes[sizeof(long)-1] == 1 ? "format binary_big_endian 1.0" : "format binary_little_endian 1.0";
 	};
 
+	auto line() {
+		std::string string;
+		string.reserve(1000);
+		for (std::getline(ply, string); string.rfind("comment", 0) == 0;)
+			std::getline(ply, string);
+		return string;
+	}
+
+	void expect(const std::string words) {
+		if (line() != words)
+			throw std::runtime_error("unable to process PLY file");
+	}
+
+	template <typename T>
+	void expect(const std::string words, T &t) {
+		auto string = line();
+		if (string.rfind(words, 0) != 0)
+			throw std::runtime_error("unable to process PLY file");
+		std::istringstream(string.erase(0, words.size())) >> t;
+	}
+
 public:
 	PLY(const std::string &ply_path) {
 		ply.exceptions(ply.exceptions() | std::ifstream::failbit);
 		ply.open(ply_path, std::ios::binary);
 
-		std::string line(4, ' ');
-		ply.read(line.data(), 4);
-		if (line != "ply\n")
-			throw std::runtime_error("not a valid PLY file");
-
-		std::getline(ply, line);
-		if (line != format_string())
-			throw std::runtime_error("not a native binary PLY file");
-
-		for (;;) {
-			std::string command, type;
-			std::getline(ply, line);
-			std::istringstream words(line);
-			words >> command;
-			if ("end_header" == command)
-				break;
-			if ("element" != command)
-				continue;
-			words >> type;
-			words >> (type == "vertex" ? vertex_count : face_count);
-		}
+		expect("ply");
+		expect(format_string());
+		expect("element vertex", vertex_count);
+		expect("property float64 x");
+		expect("property float64 y");
+		expect("property float64 z");
+		expect("property uint8 classification");
+		expect("element face", face_count);
+		expect("property list uint8 uint32 vertex_indices");
+		expect("end_header");
 
 		points.reserve(vertex_count);
 		for (std::size_t index = 0; index < vertex_count; ++index)
