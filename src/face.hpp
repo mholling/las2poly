30631a7c11cbb9f366396c7b8cfd71e74057b0dc
Matthew Hollingworth <mdholling@gmail.com>
2021-03-01 21:37:12 +1100

    simplify Face class since we're not storing them

--- src/face.hpp
+++ src/face.hpp
@@ -1,45 +1,21 @@
 #ifndef FACE_HPP
 #define FACE_HPP
 
-#include "point.hpp"
 #include "edge.hpp"
 #include <array>
-#include <functional>
 #include <cstddef>
 
-class Face {
-	std::array<Point, 3> points;
+using Face = std::array<Edge, 3>;
 
-	template <typename Iterator>
-	struct EdgeIterator {
-		const Iterator start;
-		Iterator here;
-
-		EdgeIterator(Iterator start, Iterator here) : start(start), here(here) { }
-		auto &operator++() { ++here; return *this;}
-		auto operator++(int) { auto old = *this; ++here; return old;}
-		auto operator!=(EdgeIterator other) const { return here != other.here; }
-		auto operator*() { return Edge(*here, *(here - start == 2 ? start : here + 1)); }
-	};
-
-public:
-	auto begin() const { return EdgeIterator(points.begin(), points.begin()); }
-	auto   end() const { return EdgeIterator(points.begin(), points.end()); }
-
-	friend auto operator==(const Face &face1, const Face &face2) {
-		return face1.points == face2.points;
-	}
-
-	auto operator>(double length) const {
-		for (const auto edge: *this)
-			if (edge > length)
-				return true;
-		return false;
-	}
+auto operator>(const Face &face, double length) {
+	for (const auto edge: face)
+		if (edge > length)
+			return true;
+	return false;
 };
 
 template <> struct std::hash<Face> {
-	std::size_t operator()(const Face &face) const { return hash<Edge>()(*face.begin()); }
+	std::size_t operator()(const Face &face) const { return hash<Edge>()(face[0]); }
 };
 
 #endif
