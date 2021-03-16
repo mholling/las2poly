db8b98f867ce5fec6704d5a407b115fe6c9825d3
Matthew Hollingworth <mdholling@gmail.com>
2021-03-17 09:08:56 +1100

    Ring derives from its vertex list

--- src/ring.hpp
+++ src/ring.hpp
@@ -11,29 +11,31 @@
 #include <algorithm>
 #include <ostream>
 
-class Ring {
+class Ring : std::list<Vector<2>> {
 	using Vertex = Vector<2>;
-	using Corner = std::tuple<const Vertex &, const Vertex &, const Vertex &>;
 	using Vertices = std::list<Vertex>;
 	using VertexIterator = typename Vertices::const_iterator;
+	using Corner = std::tuple<const Vertex &, const Vertex &, const Vertex &>;
 
-	Vertices vertices;
 	double signed_area;
 
 	struct VertexOnRing : std::runtime_error {
 		VertexOnRing() : runtime_error("vertex on ring") { }
 	};
 
+	auto begin() const { return Iterator(*this, Vertices::begin()); }
+	auto   end() const { return Iterator(*this, Vertices::end()); }
+
 	struct Iterator {
-		const Vertices &vertices;
+		const Ring &ring;
 		VertexIterator here;
 
-		Iterator(const Vertices &vertices, VertexIterator here) : vertices(vertices), here(here) { }
+		Iterator(const Ring &ring, VertexIterator here) : ring(ring), here(here) { }
 		auto &operator++() { ++here; return *this;}
 		auto &operator--() { --here; return *this;}
 		auto operator!=(Iterator other) const { return here != other.here; }
-		auto next() const { return here == --vertices.end() ? Iterator(vertices, vertices.begin()) : ++Iterator(vertices, here); }
-		auto prev() const { return here == vertices.begin() ? --Iterator(vertices, vertices.end()) : --Iterator(vertices, here); }
+		auto next() const { return *this != --ring.end() ? ++Iterator(ring, here) : ring.begin(); }
+		auto prev() const { return *this != ring.begin() ? --Iterator(ring, here) : --ring.end(); }
 		operator VertexIterator() const { return here; }
 		operator const Vertex &() const { return *here; }
 		auto operator*() const { return Corner(prev(), *here, next()); }
@@ -47,8 +49,9 @@ class Ring {
 		}
 	};
 
-	auto begin() const { return Iterator(vertices, vertices.begin()); }
-	auto   end() const { return Iterator(vertices, vertices.end()); }
+	const Vertices &vertices() const {
+		return *this;
+	}
 
 	auto winding_number(const Vertex &v) const {
 		int winding = 0;
@@ -67,14 +70,14 @@ public:
 	Ring(const Edges &edges) : signed_area(0) {
 		const auto &p = edges.begin()->first;
 		for (const auto &[p1, p2]: edges) {
-			vertices.push_back(p1);
+			push_back(p1);
 			signed_area += (p1 - p) ^ (p2 - p);
 		}
 		signed_area *= 0.5;
 	}
 
 	auto contains(const Ring &ring) const {
-		for (const auto &vertex: ring.vertices)
+		for (const auto &vertex: ring.vertices())
 			try { return winding_number(vertex) != 0; }
 			catch (VertexOnRing &) { }
 		return false; // ring == *this
@@ -94,7 +97,7 @@ public:
 			corners.erase(corner);
 			corners.erase(prev);
 			corners.erase(next);
-			vertices.erase(corner);
+			erase(corner);
 			corners.insert(next.prev());
 			corners.insert(prev.next());
 		}
@@ -103,7 +106,7 @@ public:
 	void smooth(double tolerance, double angle) {
 		static constexpr auto pi = 3.14159265358979323846264338327950288419716939937510;
 		auto cosine = std::cos(pi * angle / 180.0);
-		for (Vertices smoothed; smoothed.size() != vertices.size(); vertices.swap(smoothed)) {
+		for (Vertices smoothed; smoothed.size() != size(); swap(smoothed)) {
 			smoothed.clear();
 			for (const auto &[v0, v1, v2]: *this) {
 				auto d0 = v1 - v0;
@@ -136,9 +139,9 @@ public:
 
 	friend auto &operator<<(std::ostream &json, const Ring &ring) {
 		json << '[';
-		for (const auto &vertex: ring.vertices)
+		for (const auto &vertex: ring.vertices())
 			json << vertex << ',';
-		return json << ring.vertices.front() << ']';
+		return json << ring.front() << ']';
 	}
 };
 
