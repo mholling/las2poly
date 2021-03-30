4a0f7061ad4255647a4458070957d0f2c18f7a4a
Matthew Hollingworth <mdholling@gmail.com>
2021-03-30 15:32:31 +1100

    don't expose --simplify and --smooth tolerance options

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
 	static constexpr auto pi = 3.14159265358979323846264338327950288419716939937510;
 	static constexpr auto max_web_mercator = 20048966.10;
 	static constexpr auto min_resolution = max_web_mercator / INT32_MAX;
+	static constexpr auto smoothing_angle = 15.0;
 
 	try {
 		std::optional<double> width;
@@ -28,10 +29,8 @@ int main(int argc, char *argv[]) {
 		std::optional<double> area;
 		std::optional<double> length;
 		std::optional<double> resolution;
-		std::optional<double> simplify;
-		std::optional<double> smooth;
-		std::optional<bool> automatic;
-		std::optional<double> angle = 15;
+		std::optional<bool> simplify;
+		std::optional<bool> smooth;
 		std::optional<std::vector<int>> classes;
 		std::optional<int> epsg;
 		std::optional<int> threads = std::max(1u, std::thread::hardware_concurrency());
@@ -47,10 +46,8 @@ int main(int argc, char *argv[]) {
 		args.option("-a", "--area",       "<metres²>",   " minimum waterbody and island area",     area);
 		args.option("-l", "--length",     "<metres>",    "minimum edge length for void triangles", length);
 		args.option("-r", "--resolution", "<metres>",    "point thinning resolution",              resolution);
-		args.option("-i", "--simplify",   "<metres²>",   " output simplification tolerance",       simplify);
-		args.option("-m", "--smooth",     "<metres>",    "output smoothing tolerance",             smooth);
-		args.option("-u", "--auto",                      "automatic simplification and smoothing", automatic);
-		args.option("-g", "--angle",      "<degrees>",   "threshold smoothing angle",              angle);
+		args.option("-i", "--simplify",                  "apply output simplification",            simplify);
+		args.option("-m", "--smooth",                    "apply output smoothing",                 smooth);
 		args.option("-c", "--classes",    "<class,...>", "additional lidar point classes",         classes);
 		args.option("-e", "--epsg",       "<number>",    "EPSG code to set in output file",        epsg);
 		args.option("-t", "--threads",    "<number>",    "number of processing threads",           threads);
@@ -87,14 +84,6 @@ int main(int argc, char *argv[]) {
 			throw std::runtime_error("area can't be negative");
 		if (resolution && resolution.value() < min_resolution)
 			throw std::runtime_error("resolution value too low");
-		if (simplify && simplify.value() < 0)
-			throw std::runtime_error("simplification tolerance can't be negative");
-		if (smooth && smooth.value() < 0)
-			throw std::runtime_error("smoothing tolerance can't be negative");
-		if (angle.value() <= 0)
-			throw std::runtime_error("smoothing angle must be positive");
-		if (angle.value() >= 180)
-			throw std::runtime_error("smoothing angle must be less than 180");
 		for (auto klass: classes.value()) {
 			if (klass < 0 || klass > 255)
 				throw std::runtime_error("invalid lidar point class " + std::to_string(klass));
@@ -119,18 +108,8 @@ int main(int argc, char *argv[]) {
 			area = 4 * width.value() * width.value();
 		if (!resolution)
 			resolution = length.value() / std::sqrt(8.0);
-		if (automatic && !simplify)
-			simplify = 4 * width.value() * width.value();
-		if (automatic && !smooth)
-			smooth = 0.25 * std::sqrt(simplify.value()) / std::sin(angle.value() * pi / 180);
 
 		auto logger = Logger((bool)progress);
-
-		if (automatic) {
-			logger.info("simplification tolerance: ", simplify.value(), "m²");
-			logger.info("smoothing tolerance: ", smooth.value(), "m");
-			logger.info("smoothing angle: ", angle.value(), "°");
-		}
 
 		logger.time("reading", tile_paths.size(), "file");
 		auto points = Points(tile_paths, resolution.value(), classes.value(), threads.value());
@@ -141,10 +120,16 @@ int main(int argc, char *argv[]) {
 		logger.time("extracting polygons");
 		auto land = Land(mesh, length.value(), width.value(), slope.value() * pi / 180, area.value(), threads.value());
 
-		if (simplify && simplify.value() > 0)
-			land.simplify(simplify.value());
-		if (smooth && smooth.value() > 0)
-			land.smooth(smooth.value(), angle.value() * pi / 180);
+		if (simplify) {
+			auto tolerance = 4 * width.value() * width.value();
+			land.simplify(tolerance);
+		}
+
+		if (smooth) {
+			auto angle = smoothing_angle * pi / 180;
+			auto tolerance = 0.5 * width.value() / std::sin(angle);
+			land.smooth(tolerance, angle);
+		}
 
 		std::stringstream json;
 		json.precision(15);
