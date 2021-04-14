670c3c4d9f242e1a48ee5a4e610f40204f27920d
Matthew Hollingworth <mdholling@gmail.com>
2021-04-14 15:45:10 +1000

    use correct exact formula for in-circle predicate

--- src/circle.hpp
+++ src/circle.hpp
@@ -3,7 +3,6 @@
 
 #include "points.hpp"
 #include "ieee754.hpp"
-#include "edge.hpp"
 #include <tuple>
 #include <cmath>
 #include <compare>
@@ -20,30 +19,51 @@ auto operator<=>(Circle const &circle, PointIterator const &point) {
 	auto static constexpr error_scale = epsilon() * (10 + 96 * epsilon());
 
 	auto const &[p1, p2, p3] = circle;
-	auto const [x1, y1] = *p1 - *point;
-	auto const [x2, y2] = *p2 - *point;
-	auto const [x3, y3] = *p3 - *point;
-	auto const dot1 = x1 * x1 + y1 * y1;
-	auto const dot2 = x2 * x2 + y2 * y2;
-	auto const dot3 = x3 * x3 + y3 * y3;
-	auto const x2y3 = x2 * y3, y2x3 = y2 * x3;
-	auto const x3y1 = x3 * y1, y3x1 = y3 * x1;
-	auto const x1y2 = x1 * y2, y1x2 = y1 * x2;
-	auto const det = dot1 * (x2y3 - y2x3) + dot2 * (x3y1 - y3x1) + dot3 * (x1y2 - y1x2);
+	auto const &[x1, y1] = *p1;
+	auto const &[x2, y2] = *p2;
+	auto const &[x3, y3] = *p3;
+	auto const &[x4, y4] = *point;
 
-	auto const error_bound = error_scale * (
-		dot1 * (abs(x2y3) + abs(y2x3)) +
-		dot2 * (abs(x3y1) + abs(y3x1)) +
-		dot3 * (abs(x1y2) + abs(y1x2))
+	auto const dx1 = x1 - x4, dy1 = y1 - y4;
+	auto const dx2 = x2 - x4, dy2 = y2 - y4;
+	auto const dx3 = x3 - x4, dy3 = y3 - y4;
+	auto const dot1 = dx1 * dx1 + dy1 * dy1;
+	auto const dot2 = dx2 * dx2 + dy2 * dy2;
+	auto const dot3 = dx3 * dx3 + dy3 * dy3;
+	auto const dx2dy3 = dx2 * dy3, dx3dy2 = dx3 * dy2;
+	auto const dx3dy1 = dx3 * dy1, dx1dy3 = dx1 * dy3;
+	auto const dx1dy2 = dx1 * dy2, dx2dy1 = dx2 * dy1;
+	auto const det = (
+		dot1 * (dx2dy3 - dx3dy2) +
+		dot2 * (dx3dy1 - dx1dy3) +
+		dot3 * (dx1dy2 - dx2dy1)
 	);
-
+	auto const error_bound = error_scale * (
+		dot1 * (abs(dx2dy3) + abs(dx3dy2)) +
+		dot2 * (abs(dx3dy1) + abs(dx1dy3)) +
+		dot3 * (abs(dx1dy2) + abs(dx2dy1))
+	);
 	if (abs(det) > error_bound)
 		return det <=> 0;
 	else {
-		auto const ea = Edge(point, p1);
-		auto const eb = Edge(point, p2);
-		auto const ec = Edge(point, p3);
-		return (ea * ea) * (eb ^ ec) + (eb * eb) * (ec ^ ea) + (ec * ec) * (ea ^ eb) <=> 0;
+		auto const dx1 = Exact(x1) - Exact(x4);
+		auto const dx2 = Exact(x2) - Exact(x4);
+		auto const dx3 = Exact(x3) - Exact(x4);
+		auto const dy1 = Exact(y1) - Exact(y4);
+		auto const dy2 = Exact(y2) - Exact(y4);
+		auto const dy3 = Exact(y3) - Exact(y4);
+		auto const dot1 = dx1 * dx1 + dy1 * dy1;
+		auto const dot2 = dx2 * dx2 + dy2 * dy2;
+		auto const dot3 = dx3 * dx3 + dy3 * dy3;
+		auto const dx2dy3 = dx2 * dy3, dx3dy2 = dx3 * dy2;
+		auto const dx3dy1 = dx3 * dy1, dx1dy3 = dx1 * dy3;
+		auto const dx1dy2 = dx1 * dy2, dx2dy1 = dx2 * dy1;
+		auto const det = (
+			dot1 * (dx2dy3 - dx3dy2) +
+			dot2 * (dx3dy1 - dx1dy3) +
+			dot3 * (dx1dy2 - dx2dy1)
+		);
+		return det <=> 0;
 	}
 }
 
