6de873f54a5898b4844db74bbeb36faac2e67319
Matthew Hollingworth <mdholling@gmail.com>
2021-04-15 14:07:30 +1000

    improve readability

--- src/circle.hpp
+++ src/circle.hpp
@@ -51,20 +51,20 @@ auto operator<=>(Circle const &circle, PointIterator const &point) {
 	auto const [y_min, y_max] = std::minmax({y1, y2, y3, y4});
 
 	if ((2 * x_min > x_max || 2 * x_max < x_min) && (2 * y_min > y_max || 2 * y_max < y_min)) {
-		auto const ex1 = Exact(dx1), ey1 = Exact(dy1);
-		auto const ex2 = Exact(dx2), ey2 = Exact(dy2);
-		auto const ex3 = Exact(dx3), ey3 = Exact(dy3);
-		auto const det1 = (ex1 * ex1 + ey1 * ey1) * (ex2 * ey3 - ex3 * ey2);
-		auto const det2 = (ex2 * ex2 + ey2 * ey2) * (ex3 * ey1 - ex1 * ey3);
-		auto const det3 = (ex3 * ex3 + ey3 * ey3) * (ex1 * ey2 - ex2 * ey1);
+		auto const dx1 = Exact(x1 - x4), dy1 = Exact(y1 - y4);
+		auto const dx2 = Exact(x2 - x4), dy2 = Exact(y2 - y4);
+		auto const dx3 = Exact(x3 - x4), dy3 = Exact(y3 - y4);
+		auto const det1 = (dx1 * dx1 + dy1 * dy1) * (dx2 * dy3 - dx3 * dy2);
+		auto const det2 = (dx2 * dx2 + dy2 * dy2) * (dx3 * dy1 - dx1 * dy3);
+		auto const det3 = (dx3 * dx3 + dy3 * dy3) * (dx1 * dy2 - dx2 * dy1);
 		return det1 + det2 + det3 <=> 0;
 	} else {
-		auto const ex1 = Exact(x1) - Exact(x4), ey1 = Exact(y1) - Exact(y4);
-		auto const ex2 = Exact(x2) - Exact(x4), ey2 = Exact(y2) - Exact(y4);
-		auto const ex3 = Exact(x3) - Exact(x4), ey3 = Exact(y3) - Exact(y4);
-		auto const det1 = (ex1 * ex1 + ey1 * ey1) * (ex2 * ey3 - ex3 * ey2);
-		auto const det2 = (ex2 * ex2 + ey2 * ey2) * (ex3 * ey1 - ex1 * ey3);
-		auto const det3 = (ex3 * ex3 + ey3 * ey3) * (ex1 * ey2 - ex2 * ey1);
+		auto const dx1 = Exact(x1) - Exact(x4), dy1 = Exact(y1) - Exact(y4);
+		auto const dx2 = Exact(x2) - Exact(x4), dy2 = Exact(y2) - Exact(y4);
+		auto const dx3 = Exact(x3) - Exact(x4), dy3 = Exact(y3) - Exact(y4);
+		auto const det1 = (dx1 * dx1 + dy1 * dy1) * (dx2 * dy3 - dx3 * dy2);
+		auto const det2 = (dx2 * dx2 + dy2 * dy2) * (dx3 * dy1 - dx1 * dy3);
+		auto const det3 = (dx3 * dx3 + dy3 * dy3) * (dx1 * dy2 - dx2 * dy1);
 		return det1 + det2 + det3 <=> 0;
 	}
 }
