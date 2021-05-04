cfb468605c9d0286fea840354a097bf14be0ae64
Matthew Hollingworth <mdholling@gmail.com>
2021-05-05 08:56:10 +1000

    refactor Segment operators

--- src/segment.hpp
+++ src/segment.hpp
@@ -13,36 +13,21 @@
 
 using Segment = std::pair<Vector<2>, Vector<2>>;
 
-auto operator&&(Segment const &s1, Segment const &s2) {
-	auto const &[u1, v1] = s1;
-	auto const &[u2, v2] = s2;
-	auto const det = (v1 - u1) ^ (v2 - u2);
-	if (det == 0)
-		return false;
-	auto const t1 = (u2 - u1) ^ (v2 - u2);
-	auto const t2 = (u2 - u1) ^ (v1 - u1);
-	if (det > 0)
-		return t1 > 0 && t1 < det && t2 > 0 && t2 < det;
-	else
-		return t1 < 0 && t1 > det && t2 < 0 && t2 > det;
-}
-
 // segment <  vector : segment lies to the left of vector
 // segment <= vector : segment lies to the left of vector or is colinear
 // segment >= vector : segment lies to the right of vector or is colinear
 // segment >  vector : segment lies to the right of vector
 
 auto operator<=>(Segment const &segment, Vector<2> const &vector) {
-	auto const &[v1, v2] = segment;
-	auto const &[x1, y1] = v1;
-	auto const &[x2, y2] = v2;
-	auto const &[x3, y3] = vector;
-
-	auto const det1 = (x2 - x1) * (y3 - y2);
-	auto const det2 = (x3 - x2) * (y2 - y1);
-	auto const det = det1 - det2;
-
+	auto const &[u, v] = segment;
+	auto const det = (vector - u) ^ (vector - v);
 	return det <=> 0;
+}
+
+auto operator&&(Segment const &s1, Segment const &s2) {
+	auto const &[u1, v1] = s1;
+	auto const &[u2, v2] = s2;
+	return s1 <= u2 != s1 <= v2 && s2 <= u1 != s2 <= v1;
 }
 
 #endif
