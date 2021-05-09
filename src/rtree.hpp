3e54a35c30ecfe1e60341660a7ef75f97cd694da
Matthew Hollingworth <mdholling@gmail.com>
2021-05-09 14:29:33 +1000

    refactor internal RTree classes

--- src/rtree.hpp
+++ src/rtree.hpp
@@ -14,223 +14,198 @@
 #include <stack>
 #include <iterator>
 #include <cstddef>
-#include <algorithm>
 #include <stdexcept>
+#include <algorithm>
 #include <vector>
 
 template <typename Element>
 class RTree {
-	struct Node {
-		using NodePtr = std::unique_ptr<Node>;
-		using Children = std::pair<NodePtr, NodePtr>;
-		using Value = std::variant<Element, Children>;
+	using RTreePtr = std::unique_ptr<RTree>;
+	using Children = std::pair<RTreePtr, RTreePtr>;
+	using Value = std::variant<Children, Element>;
 
-		Bounds bounds;
-		Value value;
+	Bounds bounds;
+	Value value;
 
-		Node(Element const &element) :
-			bounds(element.bounds()),
-			value(element)
-		{ }
+	auto is_leaf() const {
+		return std::holds_alternative<Element>(value);
+	}
 
-		Node(NodePtr &&node1, NodePtr &&node2) :
-			bounds(node1->bounds + node2->bounds),
-			value(std::in_place_type<Children>, std::move(node1), std::move(node2))
-		{ }
+	auto &children() const {
+		return std::get<Children>(value);
+	}
 
-		auto is_leaf() const {
-			return std::holds_alternative<Element>(value);
-		}
+	auto &children() {
+		return std::get<Children>(value);
+	}
 
-		auto &children() const {
-			return std::get<Children>(value);
-		}
+	auto &element() const {
+		return std::get<Element>(value);
+	}
 
-		auto &children() {
-			return std::get<Children>(value);
-		}
+	struct Search : std::stack<RTree const *> {
+		Bounds const &bounds;
 
-		auto &element() const {
-			return std::get<Element>(value);
-		}
+		struct Iterator {
+			using iterator_category = std::input_iterator_tag;
+			using value_type        = Element const;
+			using reference         = Element const &;
+			using pointer           = void;
+			using difference_type   = void;
 
-		struct Search : std::stack<Node const *> {
-			Bounds const &bounds;
+			Search &search;
+			std::size_t index;
 
-			struct Iterator {
-				using iterator_category = std::input_iterator_tag;
-				using value_type        = Element const;
-				using reference         = Element const &;
-				using pointer           = void;
-				using difference_type   = void;
-
-				Search &search;
-				std::size_t index;
-
-				auto &next() {
-					while (!search.empty()) {
-						auto const &node = *search.top();
-						if (!(node.bounds && search.bounds))
-							search.pop();
-						else if (node.is_leaf())
-							break;
-						else {
-							auto const &[node1, node2] = node.children();
-							search.pop();
-							search.push(node1.get());
-							search.push(node2.get());
-						}
+			auto &next() {
+				while (!search.empty()) {
+					auto const &rtree = *search.top();
+					if (!(rtree.bounds && search.bounds))
+						search.pop();
+					else if (rtree.is_leaf())
+						break;
+					else {
+						auto const &[rtree1, rtree2] = rtree.children();
+						search.pop();
+						search.push(rtree1.get());
+						search.push(rtree2.get());
 					}
-					index = search.empty() ? 0 : index + 1;
-					return *this;
 				}
-
-				Iterator(Search &search) : search(search), index(0) { }
-
-				auto &operator++() {
-					search.pop();
-					return next();
-				}
-
-				auto operator==(Iterator const &other) const {
-					return other.index == index;
-				}
-
-				auto operator!=(Iterator const &other) const {
-					return other.index != index;
-				}
-
-				auto &operator*() const {
-					return search.top()->element();
-				}
-			};
-
-			Search(Bounds const &bounds, Node const *root) : bounds(bounds) {
-				this->push(root);
+				index = search.empty() ? 0 : index + 1;
+				return *this;
 			}
 
-			auto begin() { return Iterator(*this).next(); }
-			auto   end() { return Iterator(*this); }
+			Iterator(Search &search) : search(search), index(0) { }
+
+			auto &operator++() {
+				search.pop();
+				return next();
+			}
+
+			auto operator==(Iterator const &other) const {
+				return other.index == index;
+			}
+
+			auto operator!=(Iterator const &other) const {
+				return other.index != index;
+			}
+
+			auto &operator*() const {
+				return search.top()->element();
+			}
 		};
 
-		auto search(Bounds const &bounds) const {
-			return Search(bounds, this);
+		Search(Bounds const &bounds, RTree const *root) : bounds(bounds) {
+			this->push(root);
 		}
 
-		enum { found_none, found_leaf, found_branch };
-
-		auto erase(Element const &element, Bounds const &element_bounds) {
-			if (is_leaf())
-				return this->element() == element ? found_leaf : found_none;
-			if (element_bounds <= bounds) {
-				auto const &[node1, node2] = children();
-				switch (node1->erase(element, element_bounds)) {
-				case found_none: break;
-				case found_branch:
-					bounds = node1->bounds + node2->bounds;
-					return found_branch;
-				case found_leaf:
-					auto &node = *node2;
-					std::swap(node, *this);
-					auto &[node1, node2] = node.children();
-					node1.reset(), node2.reset();
-					return found_branch;
-				}
-				switch (node2->erase(element, element_bounds)) {
-				case found_none: break;
-				case found_branch:
-					bounds = node1->bounds + node2->bounds;
-					return found_branch;
-				case found_leaf:
-					auto &node = *node1;
-					std::swap(node, *this);
-					auto &[node1, node2] = node.children();
-					node2.reset(), node1.reset();
-					return found_branch;
-				}
-			}
-			return found_none;
-		}
-
-		auto replace(Element const &element, Bounds const &old_bounds, Element const &element1, Element const &element2) {
-			if (is_leaf()) {
-				if (this->element() == element) {
-					auto node1 = std::make_unique<Node>(element1);
-					auto node2 = std::make_unique<Node>(element2);
-					bounds = node1->bounds + node2->bounds;
-					value = Children(std::move(node1), std::move(node2));
-					return true;
-				}
-			} else if (old_bounds <= bounds) {
-				auto const &[node1, node2] = children();
-				if (node1->replace(element, old_bounds, element1, element2) || node2->replace(element, old_bounds, element1, element2)) {
-					bounds = node1->bounds + node2->bounds;
-					return true;
-				}
-			}
-			return false;
-		}
-
-		auto update(Element const &element, Bounds const &old_bounds) {
-			if (is_leaf()) {
-				if (this->element() == element) {
-					bounds = element.bounds();
-					return true;
-				}
-			} else if (old_bounds <= bounds) {
-				auto const &[node1, node2] = children();
-				if (node1->update(element, old_bounds) || node2->update(element, old_bounds)) {
-					bounds = node1->bounds + node2->bounds;
-					return true;
-				}
-			}
-			return false;
-		}
+		auto begin() { return Iterator(*this).next(); }
+		auto   end() { return Iterator(*this); }
 	};
 
-	template <typename Iterator, bool horizontal = true>
-	auto static partition(Iterator begin, Iterator end) {
+public:
+	RTree(Element const &element) :
+		bounds(element.bounds()),
+		value(element)
+	{ }
+
+	template <typename Iterator>
+	RTree(Iterator begin, Iterator end, bool horizontal = true) {
 		switch (end - begin) {
 		case 0:
 			throw std::runtime_error("not enough points");
 		case 1:
-			return std::make_unique<Node>(*begin);
+			bounds = begin->bounds();
+			value = *begin;
+			break;
 		default:
 			auto const middle = begin + (end - begin) / 2;
-			std::nth_element(begin, middle, end, [](auto const &element1, auto const &element2) {
-				if constexpr (horizontal)
+			std::nth_element(begin, middle, end, [=](auto const &element1, auto const &element2) {
+				if (horizontal)
 					return element1.bounds().xmin < element2.bounds().xmin;
 				else
 					return element1.bounds().ymin < element2.bounds().ymin;
 			});
-			return std::make_unique<Node>(
-				partition<Iterator, !horizontal>(begin, middle),
-				partition<Iterator, !horizontal>(middle, end)
-			);
+			auto rtree1 = std::make_unique<RTree>(begin, middle, !horizontal);
+			auto rtree2 = std::make_unique<RTree>(middle,   end, !horizontal);
+			bounds = rtree1->bounds + rtree2->bounds;
+			value = Children(std::move(rtree1), std::move(rtree2));
 		}
 	}
 
-	using NodePtr = std::unique_ptr<Node>;
-
-	NodePtr root;
-
-public:
-	RTree(std::vector<Element> &elements) : root(partition(elements.begin(), elements.end())) { }
+	RTree(std::vector<Element> &elements) : RTree(elements.begin(), elements.end()) { }
 
 	auto search(Bounds const &bounds) const {
-		return root->search(bounds);
+		return Search(bounds, this);
 	}
 
-	void erase(Element const &element, Bounds const &old_bounds) {
-		root->erase(element, old_bounds);
+	enum { found_none = 0, found_leaf, found_branch };
+
+	auto erase(Element const &element, Bounds const &element_bounds) {
+		if (is_leaf())
+			return this->element() == element ? found_leaf : found_none;
+		if (element_bounds <= bounds) {
+			auto const &[rtree1, rtree2] = children();
+			switch (rtree1->erase(element, element_bounds)) {
+			case found_none: break;
+			case found_branch:
+				bounds = rtree1->bounds + rtree2->bounds;
+				return found_branch;
+			case found_leaf:
+				auto &rtree = *rtree2;
+				std::swap(rtree, *this);
+				auto &[rtree1, rtree2] = rtree.children();
+				rtree1.reset(), rtree2.reset();
+				return found_branch;
+			}
+			switch (rtree2->erase(element, element_bounds)) {
+			case found_none: break;
+			case found_branch:
+				bounds = rtree1->bounds + rtree2->bounds;
+				return found_branch;
+			case found_leaf:
+				auto &rtree = *rtree1;
+				std::swap(rtree, *this);
+				auto &[rtree1, rtree2] = rtree.children();
+				rtree2.reset(), rtree1.reset();
+				return found_branch;
+			}
+		}
+		return found_none;
 	}
 
-	void replace(Element const &element, Bounds const &old_bounds, Element const &element1, Element const &element2) {
-		root->replace(element, old_bounds, element1, element2);
+	auto replace(Element const &element, Bounds const &old_bounds, Element const &element1, Element const &element2) {
+		if (is_leaf()) {
+			if (this->element() == element) {
+				auto rtree1 = std::make_unique<RTree>(element1);
+				auto rtree2 = std::make_unique<RTree>(element2);
+				bounds = rtree1->bounds + rtree2->bounds;
+				value = Children(std::move(rtree1), std::move(rtree2));
+				return true;
+			}
+		} else if (old_bounds <= bounds) {
+			auto const &[rtree1, rtree2] = children();
+			if (rtree1->replace(element, old_bounds, element1, element2) || rtree2->replace(element, old_bounds, element1, element2)) {
+				bounds = rtree1->bounds + rtree2->bounds;
+				return true;
+			}
+		}
+		return false;
 	}
 
-	void update(Element const &element, Bounds const &old_bounds) {
-		root->update(element, old_bounds);
+	auto update(Element const &element, Bounds const &old_bounds) {
+		if (is_leaf()) {
+			if (this->element() == element) {
+				bounds = element.bounds();
+				return true;
+			}
+		} else if (old_bounds <= bounds) {
+			auto const &[rtree1, rtree2] = children();
+			if (rtree1->update(element, old_bounds) || rtree2->update(element, old_bounds)) {
+				bounds = rtree1->bounds + rtree2->bounds;
+				return true;
+			}
+		}
+		return false;
 	}
 };
 
