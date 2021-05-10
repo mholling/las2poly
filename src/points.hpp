361b3dcf9ae9f4b60cc7e994ff66a96c591b5c29
Matthew Hollingworth <mdholling@gmail.com>
2021-05-10 12:19:07 +1000

    refactor Points constructor

--- src/points.hpp
+++ src/points.hpp
@@ -28,6 +28,7 @@
 
 class Points : public std::vector<Point> {
 	using Paths = std::vector<std::string>;
+	using PathIterator = Paths::const_iterator;
 	using Discard = std::unordered_set<unsigned char>;
 
 	std::vector<Bounds> tile_bounds;
@@ -41,6 +42,11 @@ class Points : public std::vector<Point> {
 
 	void update(Points &points) {
 		tile_bounds.insert(tile_bounds.end(), points.tile_bounds.begin(), points.tile_bounds.end());
+	}
+
+	void swap(Points &other) {
+		vector::swap(other);
+		std::swap(tile_bounds, other.tile_bounds);
 	}
 
 	struct Thin {
@@ -58,8 +64,7 @@ class Points : public std::vector<Point> {
 				std::pair<int, int>(p2[0] / resolution, p2[1] / resolution);
 		}
 
-		auto operator()(Tile &&tile, Discard const &discard) const {
-			auto points = Points();
+		void operator()(Points &points, Tile &&tile, Discard const &discard) const {
 			points.reserve(tile.size());
 
 			for (auto const point: tile)
@@ -74,14 +79,12 @@ class Points : public std::vector<Point> {
 				*here = *std::min_element(range_begin, range_end, std::greater());
 				range_begin = range_end;
 			}
-			points.erase(here, points_end);
 
+			points.erase(here, points_end);
 			points.update(tile);
-			return points;
 		}
 
-		auto operator()(Points &points1, Points &points2) const {
-			auto points = Points();
+		void operator()(Points &points, Points &points1, Points &points2) const {
 			points.reserve(points1.size() + points2.size());
 
 			for (auto here1 = points1.begin(), here2 = points2.begin(), end1 = points1.end(), end2 = points2.end(); here1 != end1 || here2 != end2; ) {
@@ -97,82 +100,63 @@ class Points : public std::vector<Point> {
 
 			points.update(points1);
 			points.update(points2);
-			return points;
 		}
 	};
 
-	class Load {
-		using PathIterator = Paths::const_iterator;
-
-		Thin thin;
-		Discard discard;
-		std::mutex mutex;
-		std::exception_ptr exception;
-
-		auto operator()(PathIterator begin, PathIterator end, unsigned threads) {
-			if (auto lock = std::lock_guard(mutex); exception)
-				return Points();
-			auto const middle = begin + (end - begin) / 2;
-			if (begin + 1 == end)
+	Points(PathIterator begin, PathIterator end, double resolution, Discard const &discard, std::mutex &mutex, std::exception_ptr &exception, unsigned threads) {
+		if (auto lock = std::lock_guard(mutex); exception)
+			return;
+		auto thin = Thin(resolution);
+		auto const middle = begin + (end - begin) / 2;
+		if (begin + 1 == end)
+			try {
+				auto const &path = *begin;
 				try {
-					auto const &path = *begin;
-					try {
-						if (path == "-") {
-							std::cin.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-							return thin(Tile(std::cin), discard);
-						} else {
-							auto input = std::ifstream(path, std::ios::binary);
-							input.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-							return thin(Tile(input), discard);
-						}
-					} catch (std::ios_base::failure &) {
-						throw std::runtime_error(path + ": problem reading file");
-					} catch (std::runtime_error &error) {
-						throw std::runtime_error(path + ": " + error.what());
+					if (path == "-") {
+						std::cin.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+						thin(*this, Tile(std::cin), discard);
+					} else {
+						auto input = std::ifstream(path, std::ios::binary);
+						input.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+						thin(*this, Tile(input), discard);
 					}
-				} catch (std::runtime_error &) {
-					auto lock = std::lock_guard(mutex);
-					exception = std::current_exception();
-					return Points();
+				} catch (std::ios_base::failure &) {
+					throw std::runtime_error(path + ": problem reading file");
+				} catch (std::runtime_error &error) {
+					throw std::runtime_error(path + ": " + error.what());
 				}
-			else if (1 == threads) {
-				auto points1 = (*this)(begin, middle, 1);
-				auto points2 = (*this)(middle, end, 1);
-				return thin(points1, points2);
-			} else {
-				auto points1 = Points();
-				auto points2 = Points();
-				auto thread1 = std::thread([&]() {
-					points1 = (*this)(begin, middle, threads/2);
-				}), thread2 = std::thread([&]() {
-					points2 = (*this)(middle, end, threads - threads/2);
-				});
-				thread1.join(), thread2.join();
-				return thin(points1, points2);
+			} catch (std::runtime_error &) {
+				auto lock = std::lock_guard(mutex);
+				exception = std::current_exception();
+				return;
 			}
+		else if (1 == threads) {
+			auto points1 = Points(begin, middle, resolution, discard, mutex, exception, 1);
+			auto points2 = Points(middle, end, resolution, discard, mutex, exception, 1);
+			thin(*this, points1, points2);
+		} else {
+			auto points1 = Points();
+			auto points2 = Points();
+			auto thread1 = std::thread([&]() {
+				Points(begin, middle, resolution, discard, mutex, exception, threads/2).swap(points1);
+			}), thread2 = std::thread([&]() {
+				Points(middle, end, resolution, discard, mutex, exception, threads - threads/2).swap(points2);
+			});
+			thread1.join(), thread2.join();
+			thin(*this, points1, points2);
 		}
-
-	public:
-		auto operator()(Paths const &paths, unsigned threads) {
-			auto points = (*this)(paths.begin(), paths.end(), threads);
-			if (exception)
-				std::rethrow_exception(exception);
-			return points;
-		}
-
-		template <typename Discard>
-		Load(double resolution, Discard const &discard) : thin(resolution), discard(discard.begin(), discard.end()) { }
-	};
-
-	auto swap(Points &other) {
-		vector::swap(other);
-		std::swap(tile_bounds, other.tile_bounds);
 	}
 
 public:
-	template <typename Discard>
-	Points(Paths const &tile_paths, double resolution, Discard const &discard, bool water, unsigned threads) {
-		Load(resolution, discard)(tile_paths, threads).swap(*this);
+	Points(Paths const &tile_paths, double resolution, std::vector<int> const &discard_ints, bool water, unsigned threads) {
+		auto discard = Discard(discard_ints.begin(), discard_ints.end());
+		auto mutex = std::mutex();
+		auto exception = std::exception_ptr();
+
+		Points(tile_paths.begin(), tile_paths.end(), resolution, discard, mutex, exception, threads).swap(*this);
+
+		if (exception)
+			std::rethrow_exception(exception);
 
 		if (water && size() > 2) {
 			auto const overall_bounds = std::accumulate(tile_bounds.begin(), tile_bounds.end(), Bounds());
