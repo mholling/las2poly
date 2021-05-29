dc2923f80c2113fa67f5a09e97da8b58de8ec8aa
Matthew Hollingworth <mdholling@gmail.com>
2021-05-29 19:47:44 +1000

    succint point ranking

--- src/point.hpp
+++ src/point.hpp
@@ -40,8 +40,8 @@ auto operator+(Point const &point) {
 
 auto operator>(Point const &p1, Point const &p2) {
 	return
-		std::tuple(p1.key_point, 2 == p1.classification ? 2 : 8 == p1.classification ? 2 : 3 == p1.classification ? 1 : 0, p2.elevation) >
-		std::tuple(p2.key_point, 2 == p2.classification ? 2 : 8 == p2.classification ? 2 : 3 == p2.classification ? 1 : 0, p1.elevation);
+		std::tuple(p1.key_point, p1.ground(), p2.elevation) >
+		std::tuple(p2.key_point, p2.ground(), p1.elevation);
 }
 
 template <>
