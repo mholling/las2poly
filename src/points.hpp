0d33f4c834ec88882b52a35902e2dfeef5191ba7
Matthew Hollingworth <mdholling@gmail.com>
2024-03-17 11:38:15 +1100

    extract Points::Thin to separate file

--- src/points.hpp
+++ src/points.hpp
@@ -10,22 +10,18 @@
 #include "point.hpp"
 #include "bounds.hpp"
 #include "srs.hpp"
-#include "tile.hpp"
 #include "app.hpp"
+#include "thin.hpp"
+#include "tile.hpp"
 #include "fill.hpp"
 #include <vector>
 #include <filesystem>
 #include <unordered_set>
 #include <set>
-#include <limits>
-#include <stdexcept>
-#include <utility>
-#include <algorithm>
-#include <functional>
-#include <iterator>
-#include <mutex>
 #include <iostream>
-#include <fstream>
+#include <mutex>
+#include <exception>
+#include <stdexcept>
 #include <thread>
 #include <cmath>
 #include <numeric>
@@ -39,77 +35,6 @@ class Points : public std::vector<Point> {
 
 	std::vector<Bounds> tile_bounds;
 	std::set<OptionalSRS> distinct_srs;
-
-	void update(Tile &tile) {
-		if (tile.bounds.empty())
-			return;
-		tile_bounds.push_back(tile.bounds);
-		distinct_srs.insert(tile.srs());
-	}
-
-	void update(Points &points) {
-		tile_bounds.insert(tile_bounds.end(), points.tile_bounds.begin(), points.tile_bounds.end());
-		distinct_srs.insert(points.distinct_srs.begin(), points.distinct_srs.end());
-	}
-
-	struct Thin {
-		auto static constexpr web_mercator_range = 40097932.2;
-		auto static constexpr min_resolution = web_mercator_range / std::numeric_limits<int>::max();
-
-		double resolution;
-
-		Thin(double resolution = min_resolution) : resolution(resolution) {
-			if (resolution < min_resolution)
-				throw std::runtime_error("width value too small");
-		}
-
-		auto operator()(Point const &p1, Point const &p2) const {
-			return
-				std::pair<int, int>(p1[0] / resolution, p1[1] / resolution) <
-				std::pair<int, int>(p2[0] / resolution, p2[1] / resolution);
-		}
-
-		void operator()(App const &app, Points &points, Tile &&tile) const {
-			points.reserve(tile.size());
-
-			for (auto const point: tile)
-				if (!point.withheld && (point.key_point || !app.discard.contains(point.classification)))
-					points.push_back(point);
-			std::sort(points.begin(), points.end(), *this);
-
-			auto here = points.begin();
-			auto const points_end = points.end();
-			for (auto range_begin = points.begin(); range_begin != points_end; ++here) {
-				auto const range_end = std::upper_bound(range_begin, points_end, *range_begin, *this);
-				*here = *std::min_element(range_begin, range_end, std::greater());
-				range_begin = range_end;
-			}
-
-			points.erase(here, points_end);
-			points.update(tile);
-		}
-
-		void operator()(Points &points, Points &points1, Points &points2) const {
-			points.reserve(points1.size() + points2.size());
-
-			auto point1 = points1.begin(), point2 = points2.begin();
-			auto const end1 = points1.end(), end2 = points2.end();
-			while (point1 != end1 && point2 != end2)
-				if ((*this)(*point1, *point2))
-					points.push_back(*point1++);
-				else if ((*this)(*point2, *point1))
-					points.push_back(*point2++);
-				else {
-					points.push_back(*point1 > *point2 ? *point1 : *point2);
-					++point1, ++point2;
-				}
-			std::copy(point1, end1, std::back_inserter(points));
-			std::copy(point2, end2, std::back_inserter(points));
-
-			points.update(points1);
-			points.update(points2);
-		}
-	};
 
 	void load(App const &app, Path const &path, Thin const &thin) {
 		if (path == "-") {
@@ -192,6 +117,18 @@ public:
 
 			fill(*this);
 		}
+	}
+
+	void update(Tile &tile) {
+		if (tile.bounds.empty())
+			return;
+		tile_bounds.push_back(tile.bounds);
+		distinct_srs.insert(tile.srs());
+	}
+
+	void update(Points &points) {
+		tile_bounds.insert(tile_bounds.end(), points.tile_bounds.begin(), points.tile_bounds.end());
+		distinct_srs.insert(points.distinct_srs.begin(), points.distinct_srs.end());
 	}
 
 	auto srs() const {
---
+++ src/thin.hpp
@@ -0,0 +1,81 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2021 Matthew Hollingworth.
+// Distributed under GNU General Public License version 3.
+// See LICENSE file for full license information.
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef THIN_HPP
+#define THIN_HPP
+
+#include "point.hpp"
+#include "app.hpp"
+#include "tile.hpp"
+#include <limits>
+#include <stdexcept>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+struct Thin {
+	auto static constexpr web_mercator_range = 40097932.2;
+	auto static constexpr min_resolution = web_mercator_range / std::numeric_limits<int>::max();
+
+	double resolution;
+
+	Thin(double resolution = min_resolution) : resolution(resolution) {
+		if (resolution < min_resolution)
+			throw std::runtime_error("width value too small");
+	}
+
+	auto operator()(Point const &p1, Point const &p2) const {
+		return
+			std::pair<int, int>(p1[0] / resolution, p1[1] / resolution) <
+			std::pair<int, int>(p2[0] / resolution, p2[1] / resolution);
+	}
+
+	template <typename Points>
+	void operator()(App const &app, Points &points, Tile &&tile) const {
+		points.reserve(tile.size());
+
+		for (auto const point: tile)
+			if (!point.withheld && (point.key_point || !app.discard.contains(point.classification)))
+				points.push_back(point);
+		std::sort(points.begin(), points.end(), *this);
+
+		auto here = points.begin();
+		auto const points_end = points.end();
+		for (auto range_begin = points.begin(); range_begin != points_end; ++here) {
+			auto const range_end = std::upper_bound(range_begin, points_end, *range_begin, *this);
+			*here = *std::min_element(range_begin, range_end, std::greater());
+			range_begin = range_end;
+		}
+
+		points.erase(here, points_end);
+		points.update(tile);
+	}
+
+	template <typename Points>
+	void operator()(Points &points, Points &points1, Points &points2) const {
+		points.reserve(points1.size() + points2.size());
+
+		auto point1 = points1.begin(), point2 = points2.begin();
+		auto const end1 = points1.end(), end2 = points2.end();
+		while (point1 != end1 && point2 != end2)
+			if ((*this)(*point1, *point2))
+				points.push_back(*point1++);
+			else if ((*this)(*point2, *point1))
+				points.push_back(*point2++);
+			else {
+				points.push_back(*point1 > *point2 ? *point1 : *point2);
+				++point1, ++point2;
+			}
+		std::copy(point1, end1, std::back_inserter(points));
+		std::copy(point2, end2, std::back_inserter(points));
+
+		points.update(points1);
+		points.update(points2);
+	}
+};
+
+#endif
